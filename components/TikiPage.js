d1b1494d822cb0b0d7b971a9287a43df13918bf0
HoCongViet2501 <viet.forwork.2501@gmail.com>
2022-09-23 10:06:32 +0700

    first commit

---
+++ components/TikiPage.js
@@ -0,0 +1,19 @@
+import { Box, Text, VStack } from "@react-native-material/core";
+import { Image, View } from "react-native";
+
+export default function TikiPage() {
+    return (
+        <VStack m={5} spacing={4} divider={true}>
+            <Box h={40}>
+                <View style={{ flex: 1, flexDirection: "column" }}>
+                    <Image source={require('../assets/book.png')} />
+                </View>
+                <View style={{ flex: 1, flexDirection: "column" }}>
+                    <Text style={{ fontSize: 15, fontWeight: "bold" }}>Nguyen ham tich phan va ung dung</Text>
+                    <Text style={{ fontSize: 15, fontWeight: "bold" }}>Nguyen ham tich phan va ung dung</Text>
+                    <Text style={{ fontSize: 18, fontWeight: "bold", marginTop: 10, color: "red" }}>148.000D</Text>
+                </View>
+            </Box>
+        </VStack>
+    );
+}
